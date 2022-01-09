d4f2d2edb8b2e342b3c22d7d43324d5c8a106053
faruk-alam <farukinnfo@gmail.com>
2022-01-09 17:31:00 +0100

    file completed

---
+++ main.js
@@ -0,0 +1,45 @@
+
+function addToplay1(){
+    var firstItem = document.querySelector(".added1");
+    firstItem.innerHTML= "<li >Spider-Man: No Way Home </li>";
+}
+function addToplay2(){
+    var secondItem = document.querySelector(".added2");
+    secondItem.innerHTML= "<li >Palm Springs </li>";
+}
+function addToplay3(){
+    var thirdItem = document.querySelector(".added3");
+    thirdItem.innerHTML= "<li >The Mitchells Vs. The Machines </li>";
+}
+function addToplay4(){
+    var fourthItem = document.querySelector(".added4");
+    fourthItem.innerHTML= "<li >The Power Of The Dog</li>";
+}
+function addToplay5(){
+    var fifthItem = document.querySelector(".added5");
+    fifthItem.innerHTML= "<li >Barb & Star Go To Vista Del Mar </li>";
+}
+function remove1(){
+    var firstRemove = document.querySelector(".name1");
+    firstRemove.style.display ="none";
+}
+function remove2(){
+    var secondRemove = document.querySelector(".name2");
+    secondRemove.style.display ="none";
+}
+function remove3(){
+    var thirdRemove = document.querySelector(".name3");
+    thirdRemove.style.display ="none";
+}
+function remove4(){
+    var fourthRemove = document.querySelector(".name4");
+    fourthRemove.style.display ="none";
+}
+function remove5(){
+    var fifthRemove = document.querySelector(".name5");
+    fifthRemove.style.display ="none";
+}
+function clearall(){
+    var listRemove = document.querySelector(".removeall");
+    listRemove.style.display ="none";
+}
